023a8a65da3ad30e95318fa54d3bc74629241f8f
kscsd1116-arch <kscsd1116@gmail.com>
2026-09-02 14:14:31 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 
 app.get("/", (req, res) => {
-  res.json({
-    status: "ok",
-    message: "PlayMaker AI API is running"
-  });
+  res.sendFile(process.cwd() + "/index.html");
 });
 
 app.get("/health", (req, res) => {
